bedfccaf8a3cd1d5b7e61bbb24f0e20510d3dcaa
hrishikesh8904 <hrishikesh.sane89@outlook.com>
2024-04-13 20:57:05 +0530

    modified:   src/components/Articles.jsx

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -12,6 +12,7 @@ function Articles() {
       <div className="articles-two">
         <div className="articles-two-1">
           <h1>Articles with blos</h1>
+          <h1>Articles with blos</h1>
         </div>
         <div className="articles-two-2">
           <BlogCard />
